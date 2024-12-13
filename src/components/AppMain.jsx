33d56a39e573e3c208f7859ea6239a94e946a012
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-12-13 17:22:15 +0100

    feat: first commit

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,12 +1,24 @@
 import style from "./AppMain.module.css";
 import PostCard from "./PostCard";
-
+import posts from "../data";
 function AppMain() {
+
+function PrintPost() {
+    return posts.map((curPost) => (
+        curPost.published &&
+        <PostCard
+        image={curPost.image}
+        title={curPost.title}
+        content={curPost.content}
+        tags={curPost.tags.join(" ")}
+        />
+    ))
+}
 
     return (
         <div className="container">
             <section className={style.text}>
-                <PostCard />
+                <PrintPost />
             </section>
 
         </div>
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,13 +1,16 @@
 import style from "./PostCard.module.css"
 import ButtonCard from "./ButtonCard";
 
-function PostCard() {
+function PostCard({title, image, content, tags, published}) {
     return (
         <div className={style.card}>
-            <div className={style.img}>600 x 400</div>
+            <div className={style.img}>{
+                image ? (<img src={image} alt="" />) : ' Nessun immagine'
+            }</div>
+
             <div className={style.content}>
-                <h4 className={style.title}>Titolo del Post</h4>
-                <p className={style.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime facilis mollitia sed atque voluptate aliquid expedita perspiciatis inventore. Provident est molestias velit beatae odio dolorem quia deserunt dicta maxime enim?</p>
+                <h4 className={style.title}>{title} <span>{tags}</span></h4>
+                <p className={style.description}>{content}</p>
                 <ButtonCard />
             </div>
 
