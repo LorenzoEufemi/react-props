86742c519af53d9f59ac429ecdbb45fb8e905604
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-12-14 12:00:44 +0100

    feat: consegna con bonus

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -3,24 +3,39 @@ import PostCard from "./PostCard";
 import posts from "../data";
 function AppMain() {
 
-function PrintPost() {
-    return posts.map((curPost) => (
-        curPost.published &&
-        <PostCard
-        image={curPost.image}
-        title={curPost.title}
-        content={curPost.content}
-        tags={curPost.tags}
-        />
-    ))
-}
+    function PrintPost() {
+        return posts.map((curPost) => (
+            curPost.published &&
+            <PostCard
+                image={curPost.image}
+                title={curPost.title}
+                content={curPost.content}
+                tags={curPost.tags}
+            />
+        ))
+    }
 
+    function listTags() {
+        const listTag = []
+
+        posts.forEach((curPost) => {
+            curPost.tags.forEach((curTag) => {
+                if (!listTag.includes(curTag)) {
+                    listTag.push(curTag)
+                }
+            })
+
+        })
+        return listTag.join(" ")
+    }
     return (
         <div className="container">
             <section className={style.text}>
                 <PrintPost />
             </section>
-
+            <section>
+               Lista Tag : {listTags()}
+            </section>
         </div>
     )
 }
